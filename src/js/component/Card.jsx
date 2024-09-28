410895d63b36a6079bbd4a70be8d505720579b87
Emma4212 <110198302+Emma4212@users.noreply.github.com>
2024-09-28 20:03:35 +0000

    cambios 4.0

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -11,7 +11,7 @@ const Card = () => {
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 justify-content-center">
           <div className="col">
             <div className="card h-100 text-center">
-              <img src="https://picsum.photos/seed/picsum/500/325" className="card-img-top" alt="..." />
+              <img src="https://picsum.photos/seed/image1/500/325" className="card-img-top" alt="..." />
               <div className="card-body d-flex flex-column">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -25,7 +25,7 @@ const Card = () => {
 
           <div className="col">
             <div className="card h-100 text-center">
-              <img src="https://picsum.photos/seed/picsum/500/325" className="card-img-top" alt="..." />
+              <img src="https://picsum.photos/seed/image3/500/325" className="card-img-top" alt="..." />
               <div className="card-body d-flex flex-column">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">This is a short card.</p>
@@ -39,7 +39,7 @@ const Card = () => {
 
           <div className="col">
             <div className="card h-100 text-center">
-              <img src="https://picsum.photos/seed/picsum/500/325" className="card-img-top" alt="..." />
+              <img src="https://picsum.photos/seed/image2/500/325" className="card-img-top" alt="..." />
               <div className="card-body d-flex flex-column">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -53,7 +53,7 @@ const Card = () => {
 
           <div className="col">
             <div className="card h-100 text-center">
-              <img src="https://picsum.photos/seed/picsum/500/325" className="card-img-top" alt="..." />
+              <img src="https://picsum.photos/seed/image4/500/325" className="card-img-top" alt="..." />
               <div className="card-body d-flex flex-column">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">This is another short card.</p>
