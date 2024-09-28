59eec40ee5b2bbf37d824903d473cc31a8ddd510
Emma4212 <110198302+Emma4212@users.noreply.github.com>
2024-09-28 19:43:31 +0000

    cambios 2.0

---
+++ src/js/component/alerta.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Alerta = () => {
+
+  return (
+    <>
+
+
+
+
+      <div className="container bg-light text-start mt-5 p-5  ">
+        <h1 className="title">A Warm Welcome!</h1>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi,
+          in quo sunt possimus non incidunt odit vero aliquid similique quaeerat nam nobis
+          illo aspernatur vitae fugiat numquam repellat.
+        </p>
+        <a href="#" className="btn btn-primary btn-lg">Call to action!</a>
+      </div>
+
+
+
+    </>
+
+
+  )
+
+
+
+
+}
+
+export default Alerta;
